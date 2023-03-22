1ea685e43c9bb53b471e493322498ea4fb3923e7
SungChul Hong <51399982+ironAiken2@users.noreply.github.com>
2023-03-22 14:41:51 +0900

    Time: 134 ms (74.24%), Space: 16.7 MB (44.37%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,14 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        
-        
-        for i in range(0, len(nums), 2):
-            try:
-                if nums[i] != nums[i+1]:
-                    return nums[i]
-            except:
-                return nums[i]
-            
-        return nums[i]
-            
+        # Initialize the unique number...
+        uniqNum = 0;
+        # TRaverse all elements through the loop...
+        for idx in nums:
+            # Concept of XOR...
+            uniqNum ^= idx;
+        return uniqNum; 
