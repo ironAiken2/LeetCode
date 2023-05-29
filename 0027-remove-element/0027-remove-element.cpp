7c35295a26701d2a33fb4fa57e57d750ffe8aff0
SungChul Hong <hsc6263@gmail.com>
2023-05-29 23:42:49 +0900

    Time: 0 ms (100.00%), Space: 8.7 MB (89.76%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         
-        nums.erase(remove_if(nums.begin(), nums.end(), [val](auto x) -> bool { return x == as_const(val); }), nums.end());
+        nums.erase(remove_if(nums.begin(), nums.end(), [val](auto x) -> bool { return x == val; }), nums.end());
         
         
         return nums.size();
